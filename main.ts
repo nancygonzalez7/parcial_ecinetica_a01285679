38c702f1e67be11ee59e6192b25afe1cab28d17f
nancygonzalez7 <70345001+nancygonzalez7@users.noreply.github.com>
2020-09-17 14:45:05 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    let velocidad = 0
+    basic.showString("Ec=")
+    Ec = masa * velocidad
+    basic.showNumber(Ec % 2)
+})
+let Ec = 0
+let masa = 0
+basic.showString("Nancy Gonzalez A01285679")
+masa = 17.78
+let velocidad = 15
